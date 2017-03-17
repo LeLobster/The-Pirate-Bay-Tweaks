72c5e3fb5b364320244fd994f85539e23dffb0c1
LeLobster <LeLobster@users.noreply.github.com>
2017-03-17 01:40:17 +0100

    Initial upload
    jus uploading some files

---
+++ The_Pirate_Bay_Tweaks/options/options.js
@@ -0,0 +1,138 @@
+var defaultSettings = {
+    highlightGoodTor: true,
+    GoodHighlightColor: "#4bc54b",
+    GoodKeyword: [],
+    GoodUploader: [],
+    includeOther: true,
+    hideAllOtherInsteadOfHighlight: false,
+    affectx256: true,
+    x256Color: "#7585fd",
+    hidex265InsteadOfHighlight: false,
+    affectOther: true,
+    otherColor: "#aad1f3",
+    hideOtherInsteadOfHighlight: false,
+    OtherKeyword: ["hdrip", "webrip", "web-dl", "WEB"],
+    affectNonRetail: true,
+    NonRetailColor: "#841d1d",
+    hideNonRetailInsteadOfHighlight: true,
+    NonRetailKeyword: ["cam", "hdcam", "camrip", "telesync", "ts", "hdts", "hd-ts", "hardcoded", "hc",
+                       "hdtc", "dvdscr", "screener", "dvdscreener", "TRUEFRENCH", "dublado", "upscale"],
+    includeUnwanted: true,
+    affectUnwantedKeyword: true,
+    UnwantedHighlightColor: "#d73838",
+    hideUnwantedKeywordInsteadOfHighlight: false,
+    UnwantedKeyword: [],
+    affectUnwantedUploader: true,
+    UnwantedUserHighlightColor: "#d73838",
+    hideUnwantedUploaderInsteadOfHighlight: true,
+    UnwantedUploader: [],
+    hideNonMovies: false,
+    nonMovies: ["music videos", "movie clips", "3D"],
+    affectPotentialFakes: true,
+    trustedTorrentsOnly: false,
+    maxSeedsWithoutTrust: 999,
+    dontHideJustHighlightPotentialFakes: true,
+    includeUntrustedTorrentsWithoutComments: false,
+    detDescColor: "#1f2223"
+}
+
+function saveOptions(e) {
+    
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+    
+    all_forms = document.querySelectorAll('#options_form label *');
+    for (i = 0; i < all_forms.length; i++) {
+        if (all_forms[i].type === 'checkbox') {
+            id = all_forms[i].id;
+            state = document.querySelector('#' + id).checked;
+            setting = chrome.storage.local.set({ [all_forms[i].id]: state });
+        }
+        else if (all_forms[i].type === 'color') {
+            id = all_forms[i].id;
+            state = document.querySelector('#' + id).value;     
+            setting = chrome.storage.local.set({ [all_forms[i].id]: state });       
+        }
+        else if (all_forms[i].type === 'text') {
+            id = all_forms[i].id;
+            state = document.querySelector('#' + id).value.split(',');          
+            setting = chrome.storage.local.set({ [all_forms[i].id]: state }); 
+        }
+        else if (all_forms[i].type === 'number') {
+            id = all_forms[i].id;
+            state = parseInt(document.querySelector('#' + id).value);
+            setting = chrome.storage.local.set({ [all_forms[i].id]: state });       
+        }
+    }
+  e.preventDefault();
+  console.log('TPB Tweaks Web-Ext: Settings saved!');
+}
+
+function restoreDefaults() {
+    for (key in defaultSettings) {
+        element = document.querySelector('#' + key);
+        if (element.type === 'checkbox') {
+            element.checked = defaultSettings[element.id]
+        } else
+            element.value = defaultSettings[element.id]
+    }
+    console.log('TPB Tweaks Web-Ext: Default settings restored!');
+    alert('Default settings restored!');
+}
+
+function getOptions(callback) {
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+    chrome.storage.local.get(null, function(results) {
+        for (key in results) {
+            element = document.querySelector('#' + key);
+            if (element.type === 'checkbox') {
+              element.checked = results[element.id]
+            } else {
+              element.value = results[element.id]
+            }
+        }
+        console.log('TPB Tweaks Web-Ext: Settings loaded!');
+        hideOrNotToHide();
+    });
+}
+
+function hideOrNotToHide() {
+    includeOtherCheck = document.getElementById('includeOther');
+    includeUnwantedCheck = document.getElementById('includeUnwanted');
+    hiddenPelt = document.getElementById('hidden-settings');
+    hiddenPelt2 = document.getElementById('hidden-settings2');
+    hideThis = document.querySelector('#hideOrNotToHide');
+    hideThis2 = document.querySelector('#hideOrNotToHide2');
+    if (includeOtherCheck.checked === true) {
+        hideThis.style = 'display: unset;'
+        hiddenPelt.style = 'display: none;'
+        console.log('TPB Tweaks Web-Ext: includeOther section is not hidden.');
+    }
+    else {
+        hideThis.style = 'display: none;'
+        hiddenPelt.style = 'display: block;'
+        console.log('TPB Tweaks Web-Ext: includeOther section is hidden.');
+    }
+    if (includeUnwantedCheck.checked === true) {
+        hideThis2.style = 'display: unset;'
+        hiddenPelt2.style = 'display: none;'
+        console.log('TPB Tweaks Web-Ext: includeUnwanted section is not hidden.');
+    }
+    else {
+        hideThis2.style = 'display: none;'
+        hiddenPelt2.style = 'display: block;'
+        console.log('TPB Tweaks Web-Ext: includeUnwanted section is hidden.');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', getOptions);
+document.getElementById('includeOther').addEventListener('change', hideOrNotToHide);
+document.getElementById('includeUnwanted').addEventListener('change', hideOrNotToHide);
+document.querySelector('#options_form').addEventListener('submit', saveOptions);
+document.querySelector('#options_form').addEventListener('change', saveOptions);
+document.querySelector('#restore_default').addEventListener('click', restoreDefaults);
+document.querySelector('#restore_default').addEventListener('click', saveOptions);
+document.querySelector('#restore_default').addEventListener('click', hideOrNotToHide);
---
+++ The_Pirate_Bay_Tweaks/src/scripts/main.js
@@ -0,0 +1,134 @@
+﻿let userDefined = chrome.storage.local.get(null, function(userPref) {
+
+  console.log('TPB Tweaks Web-Ext: User defined preferences succesfully retrieved.');
+
+    $('td .detDesc').css('color', userPref['detDescColor']);
+    $('td .detLink').css('color', userPref['detDescColor']);
+    
+    if (userPref['highlightGoodTor']) {
+        $('#searchResult tbody tr').each(function( index ) {
+            var torrentTitle = $(this).find('td .detLink').text().search(
+                new RegExp('\\b' + userPref['GoodKeyword'].join('\\b|\\b') + '\\b', 'i'));
+            var userName = $(this).find('td .detDesc').text().search(
+                new RegExp(userPref['GoodUploader'].join('|')));
+            if (userPref['GoodKeyword'].toString() != "" && torrentTitle != -1) {
+                $(this).css('background', userPref['GoodHighlightColor']);
+            } 
+            if (userPref['GoodUploader'].toString() != "" && userName != -1) {
+                $(this).css('background', userPref['GoodHighlightColor']);
+            }
+        });
+    }
+
+    if (userPref['includeOther']) {
+        $('#searchResult tbody tr').each(function( index ) {
+            var torrentTitle = $(this).find('td .detLink').text();
+            if (userPref['hideAllOtherInsteadOfHighlight']) {
+                $(this).hide();
+            } else
+                if (userPref['affectOther']) {
+                    if (userPref['OtherKeyword'].toString() != "" && torrentTitle.search(
+                        new RegExp('\\b' + userPref['OtherKeyword'].join('\\b|\\b') + '\\b', 'i')) != -1) {
+                        if (userPref['hideOtherInsteadOfHighlight']) {
+                            $(this).hide();
+                        } else
+                            $(this).css('background', userPref['otherColor']);
+                    }
+                }
+            if (userPref['affectx256']) {
+                if (torrentTitle.search(
+                    new RegExp(/\bx265\b|\bHEVC\b|\bH265\b|\bH.265\b/ig)) != -1) {
+                    if (userPref['hidex265InsteadOfHighlight']) {
+                        $(this).hide();
+                    } else
+                        $(this).css('background', userPref['x256Color']);
+                }
+            }
+            if (userPref['affectNonRetail']) {
+                var nonRetailTorrent = torrentTitle.search(
+                    new RegExp('\\b' + userPref['NonRetailKeyword'].join('\\b|\\b') + '\\b', 'i'));
+                if (userPref['hideNonRetailInsteadOfHighlight']) {
+                    if (userPref['NonRetailKeyword'].toString() != "" && nonRetailTorrent != -1) {
+                        $(this).hide();
+                    }
+                } else
+                    if (userPref['NonRetailKeyword'].toString() != "" && nonRetailTorrent != -1) {
+                        $(this).css('background', userPref['NonRetailColor']);
+                    }
+            }
+        });
+    }
+
+    if (userPref['includeUnwanted']) {
+        $('#searchResult tbody tr').each(function( index ) {
+            var torrentTitle = $(this).find('td .detLink').text();
+            if (userPref['affectUnwantedKeyword']) {
+                var unwantedTorrent = torrentTitle.search(
+                    new RegExp('\\b' + userPref['UnwantedKeyword'].join('\\b|\\b') + '\\b', 'i'));
+                if (userPref['UnwantedKeyword'].toString() != "" && userPref['hideUnwantedKeywordInsteadOfHighlight']) {
+                    if (unwantedTorrent != -1) {
+                        $(this).hide();
+                    }
+                } else
+                    if (userPref['UnwantedKeyword'].toString() != "" && unwantedTorrent != -1) {
+                        $(this).css('background', userPref['UnwantedHighlightColor']);
+                    }
+            }
+            var userName = $(this).find('td .detDesc').text();
+            if (userPref['affectUnwantedUploader']) {
+                var unwantedUser = userName.search(
+                    new RegExp(userPref['UnwantedUploader'].join('|')));
+                if (userPref['hideUnwantedUploaderInsteadOfHighlight']) {
+                    if (userPref['UnwantedUploader'].toString() != "" && unwantedUser != -1) {
+                        $(this).hide();
+                    }
+                } else
+                    if (userPref['UnwantedUploader'].toString() != "" && unwantedUser != -1) {
+                        $(this).css('background', userPref['UnwantedUserHighlightColor']);
+                    }
+            }
+            if (userPref['hideNonMovies']) {
+                $('#searchResult tbody tr td.vertTh').each(function( index ) {
+                    var category = $(this).text()
+                    if (userPref['nonMovies'].toString() != "" && category.search(
+                        new RegExp(userPref['nonMovies'].join('\\b|\\b') + '\\b', 'i')) != -1) {               
+                        $(this.parentNode).hide();
+                    }
+                });
+            }
+        });
+    }
+
+    if (userPref['affectPotentialFakes']) {
+        $('#searchResult tbody tr:not(:has(img[title="VIP"],img[title="Trusted"],img[title="Moderator"],\
+        img[title="Helper"],td[style="text-align:center;"]))').each(function( index ) {
+            var torrentSeeds = Number( $(this).children('td:nth-child(3)').text() );
+            warningImg = chrome.extension.getURL("src/warning.png");
+            TorrentWarning = {"background-image" : "url(" + warningImg + ")",
+                              "background-position" : "95%",
+                              "background-size" : "24px",
+                              "background-repeat" : "no-repeat",
+                              "opacity" : ".5"};
+            if (userPref['trustedTorrentsOnly']) {
+                $(this).hide();
+            } else {
+                if (userPref['includeUntrustedTorrentsWithoutComments']) {
+                    if ( (torrentSeeds >= userPref['maxSeedsWithoutTrust']) || (
+                        $(this).has("img[src='//thepiratebay.org/static/img/icon_comment.gif']").length === 0) )  {
+                        if (userPref['dontHideJustHighlightPotentialFakes']) {
+                            $(this).children('td:nth-child(2)').css(TorrentWarning);
+                        } else
+                            $(this).hide();
+                    }
+                } else {
+                    if (torrentSeeds >= userPref['maxSeedsWithoutTrust']) {
+                        if (userPref['dontHideJustHighlightPotentialFakes']) {
+                            $(this).children('td:nth-child(2)').css(TorrentWarning);
+                        } else
+                            $(this).hide();
+                    }
+                }
+            }
+        });
+    }
+})
